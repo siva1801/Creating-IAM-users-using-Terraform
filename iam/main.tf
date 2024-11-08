cfd0408e4b26bea7fa5fb274d1cd9a34780ca89e
sivanesan <157710821+siva1801@users.noreply.github.com>
2024-11-08 10:23:22 +0530

    Update main.tf

--- iam/main.tf
+++ iam/main.tf
@@ -3,24 +3,17 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "iam_user" {
-  name = devops
+  name = "devops"
 }
 
 resource "aws_iam_access_key" "credentials" {
   user = aws_iam_user.iam_user.name
 }
 
-
-resource "aws_iam_user_login_profile" "credentials" {
-  user                    = aws_iam_user.iam_user.name
-  password_reset_required = true
-
 resource "aws_iam_user_login_profile" "credentials" {
   user                    = "devops-planet"
-  password                = "your-password"
   password_reset_required = true
 }
-
 
 output "password" {
   value     = aws_iam_user_login_profile.credentials.password
